a403664475ed1042113153d25442819bf1be6c07
Sidharth <sidharthsuresh07@gmail.com>
2025-09-18 16:18:20 -0400

    pygame community edition checks added

---
+++ dependency_checker.py
@@ -0,0 +1,26 @@
+"""
+This program is mainly for checking if you have all the dependencies installed.
+NOTE: This program is only to be run when you're developing the game and should not be included in the base game.
+"""
+
+import sys
+
+issue_flag = False
+
+# VE checks
+
+
+
+# Module checks
+
+def missing_module_message(display_name: str, pipi_name: str):
+    print(f"{display_name} module is not installed into this environment. Please install the module using: `pip install {pipi_name}`")
+
+try:
+    import pygame
+except ImportError:
+    missing_module_message("Pygame-CE", "pygame-ce")
+    issue_flag = True
+
+if issue_flag:
+    sys.exit("Cannot run game with these issues still persisting. Please fix the dependency issues before you run the game again.")
